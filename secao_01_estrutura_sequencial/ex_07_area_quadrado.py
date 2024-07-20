caffacd75c1b6d1af59108ce959a77ec8ee9b44a
mmpriebe <marciano.priebe@gmail.com>
2024-07-20 16:38:09 -0300

    Finalizado exercicio 7

--- secao_01_estrutura_sequencial/ex_07_area_quadrado.py
+++ secao_01_estrutura_sequencial/ex_07_area_quadrado.py
@@ -9,14 +9,20 @@ Mostrar a área com 2 casas decimais.
     >>> ex_07_area_quadrado.input = lambda k: '2'
     >>> ex_07_area_quadrado.calcular_area_de_quadrado()
     A área do quadrado com esse lado é: 4.00
-    O dobro da aŕea do quadrado é: 8.00
+    O dobro da área do quadrado é: 8.00
     >>> ex_07_area_quadrado.input = lambda k: '2.5'
     >>> ex_07_area_quadrado.calcular_area_de_quadrado()
     A área do quadrado com esse lado é: 6.25
-    O dobro da aŕea do quadrado é: 12.50
+    O dobro da área do quadrado é: 12.50
 
 """
 
 
 def calcular_area_de_quadrado():
     """Escreva aqui em baixo a sua solução"""
+    lado = float(input('Informe o lado do quadrado: '))
+    area_quadrado = lado * lado
+    dobro_da_area = area_quadrado * 2
+
+    print(f'A área do quadrado com esse lado é: {area_quadrado:2.2f}')
+    print(f'O dobro da área do quadrado é: {dobro_da_area:2.2f}')
